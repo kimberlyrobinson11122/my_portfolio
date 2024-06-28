b2e104e7756b5f90a5c982a3c5b69694216beb96
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-06-28 16:05:58 -0700

    Added Resume Page

---
+++ src/pages/ResumePage.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+// Bringing in the required component from 'react-router-dom' for linking between pages
+import { Link } from 'react-router-dom';
+
+
+export default function ResumePage() {
+  // Iterate over each mock user to display their abridged profile data and a link to their page
+  // Holding space
+  return (
+    
+    <div className="container pt-4">
+     <h1> Resume </h1>
+    </div>
+  );
+}
