6e1fa2481e0b0b8cbc2742565329d506fe3753e0
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-06-28 16:26:03 -0700

    Added app container (footer with link back to homePage)

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// Bringing in the required import from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom';
+import Nav from './components/Nav';
+
+function App() {
+  // The Outlet component will conditionally swap between the different pages according to the URL
+  return (
+    <>
+      <Nav />
+      <Outlet />
+      <footer className="profile-footer">
+        {/* Link - HomePage (instead of the href) */}
+        <Link to="/">Let's go back</Link>
+      </footer>
+    </>
+  );
+}
+
+export default App;
