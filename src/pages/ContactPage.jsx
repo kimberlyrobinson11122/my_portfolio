ccf2fe4162fc4c7957a99ab0af412e6f4325283c
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-06-28 16:21:37 -0700

    Adding ContactPage

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+// Required component from 'react-router-dom' for LINKING between pages
+import { Link } from 'react-router-dom';
+
+export default function ContactPage() {
+  
+  return (
+    
+    <div className="container pt-4">
+     <h1> Contact </h1>
+    </div>
+  );
+}
