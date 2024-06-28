aed63ce188211d95d5733a2224e194c569e68f38
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-06-28 16:26:43 -0700

    Aded index, main.jsx, and an svg (not sure if it is needed but keeping it for now)

---
+++ src/main.jsx
@@ -0,0 +1,52 @@
+import ReactDOM from 'react-dom/client';
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+// From utils brings in the pages that router will conditionally use to show the appropriate views
+import App from './App';
+import ErrorPage from './pages/ErrorPage';
+import HomePage from './pages/HomePage';
+import ProjectPage from './pages/ProjectPage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import ResumePage from './pages/ResumePage';
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'projects',
+        element: <ProjectPage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+      {
+        path: 'resume',
+        element: <ResumePage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />,
+      },
+
+    ],
+  },
+]);
+
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
