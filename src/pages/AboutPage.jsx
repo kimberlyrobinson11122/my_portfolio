04bbd8ea2b5a3656727332240a0bce295073d26c
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-06-28 16:21:50 -0700

    Adding AboutPage

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+// My About Page
+export default function AboutPage() {
+  return (
+    <div className="container pt-24  col-lg-8 m-5">
+      <p className="text-center">
+        Born and raised in the vibrant landscapes of Northern California, specifically Sunnyvale, I've always been drawn to the intersection of technology and creativity. After completing my education, I made the leap to Los Angeles, where I delved into the dynamic world of project management, honing my skills across various industries, all while nurturing a deep-seated passion for innovation. With nearly a decade of experience under my belt, I've recently embarked on a thrilling journey with an EdX and Berkeley Boot Camp, immersing myself in the latest software and design principles. Each project undertaken fuels my drive and excitement, propelling me towards the creation of impactful apps and ventures. I'm eagerly anticipating the opportunity to continue pushing boundaries and leaving my mark on the ever-evolving landscape of technology.
+      </p>
+      <section className="features-icons bg-light text-center m-4">
+        {/* Maybe some additional content for the section added here */}
+      </section>
+    </div>
+  );
+}
