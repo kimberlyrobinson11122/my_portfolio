642b437707f5b4ee0077b79248418e8bbbbc6a2e
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-06-28 16:21:16 -0700

    adding HomePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+// Required component from 'react-router-dom' for LINKING between pages
+import { Link } from 'react-router-dom';
+import Profile from '../components/UI/ProjectSections/ProfileTeaser';
+import ListItem from '../components/UI/ListItem';
+
+export default function HomePage() {
+
+  return (
+    
+    <div className="container pt-4">
+     <h1> Homepage </h1>
+    </div>
+  );
+}
