eb1059705bb30b4172c7a65becf0a2e9db2214fe
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-07-03 01:10:51 -0700

    interim commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,14 @@
-import { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function HomePage() {
   return (
     <div className="container pt-4">
-      <h1></h1>
+      <h1>Welcome to My Portfolio</h1><br />
       <h2>
-        I'm Kimberly Robinson, an aspiring designer and software developer based in Seattle, currently honing my skills. This website is a placeholder as I embark on my journey. Available for freelance work & collaborations
+        I'm Kimberly Robinson, an aspiring designer and software developer based in Seattle, currently honing my skills. This website is a placeholder as I embark on my journey. Available for freelance work & collaborations.
       </h2>
-    </div>
-
+        <img src='src/assets/images/Cover_img.jpg'/>
+      </div>
   );
 }
-
-// Add image
