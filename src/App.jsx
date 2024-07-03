2b9f8e76942318e43786482545f0d39903508fcc
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-07-02 22:27:23 -0700

    spacing

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
+    
       <Nav />
       <Outlet />
       <footer className="profile-footer">
