cbc85a628c468915b42f549f9967a82de32f0c16
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-07-08 16:51:29 -0700

    reinstalled vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/images/*',
+          dest: 'assets/images'
+        }
+      ]
+    })
+  ],
   server: {
     port: 3000,
     open: true,
