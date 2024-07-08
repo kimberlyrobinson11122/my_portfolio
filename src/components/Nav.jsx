0d50dee1d3fd2f6f83801e0dd7c24e94047a9220
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-07-07 23:22:17 -0700

    updated code

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,31 +1,15 @@
-// Must have this to require import from 'react-router-dom'
-import { Link } from 'react-router-dom';
+import React from 'react';
 import Navbar from './UI/Navbar';
 
 export default function Nav() {
-  // Src - COMPONENT - nav.jsx = Navbar UI component render each of the Link elements in the links prop
   return (
     <Navbar
       links={[
-        <Link key={1} className="nav-link text-light" to="/">
-          Home
-        </Link>,
-
-        <Link key={2} className="nav-link text-light" to="/projects">
-          Projects
-        </Link>,
-
-        <Link key={3} className="nav-link text-light" to="/resume">
-        Resume
-        </Link>,
-
-        <Link key={4} className="nav-link text-light" to="/contact">
-        Contact
-        </Link>,
-
-        <Link key={5} className="nav-link text-light" to="/about">
-        About Me
-        </Link>,
+        { key: 1, to: "/", label: "Home" },
+        { key: 2, to: "/projects", label: "Projects" },
+        { key: 3, to: "/resume", label: "Resume" },
+        { key: 4, to: "/contact", label: "Contact" },
+        { key: 5, to: "/about", label: "About Me" },
       ]}
     />
   );
