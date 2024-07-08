14211ba6e4f5d08adfee727f84c29be55ef1ebf4
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-07-07 23:23:32 -0700

    updated img to work

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './HomePage.css'; // Import your CSS file
 
 export default function HomePage() {
   return (
     <div className="container pt-4">
-      <h1>Welcome to My Portfolio</h1><br />
-      <h2>
-        I'm Kimberly Robinson, an aspiring designer and software developer based in Seattle, currently honing my skills. This website is a placeholder as I embark on my journey. Available for freelance work & collaborations.
-      </h2>
-        <img src='src/assets/images/Cover_img.jpg'/>
-      </div>
+      <h1>Welcome to My Portfolio</h1>
+      <p className="text-left" style={{ fontSize: '1.5rem' }}>
+        As an aspiring designer and software developer located in Seattle, I am dedicated to continuously refining my skills and knowledge in these fields. This website serves as a preliminary platform as I set out on my exciting journey of growth and learning. I am enthusiastic about taking on freelance opportunities and engaging in collaborative projects. I am an aspiring designer and software developer based in Seattle. Currently, I am honing my skills. This website serves as a placeholder for the start of my journey. I am open to freelance work and collaborations.
+      </p>
+      <img 
+        src="src\assets\images\Cover img.jpg" 
+        alt="Portfolio Cover" 
+        className="img-container" 
+      />
+    </div>
   );
 }
