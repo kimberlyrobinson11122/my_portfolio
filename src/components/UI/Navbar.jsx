1a740ee6e1e49abe20e463a69b5bde76153f4153
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-07-07 23:22:43 -0700

    still not working, trying to get the highlighting to work

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,10 +1,25 @@
-export default function Nav({ links }) {
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function Navbar({ links }) {
   return (
     <nav className="navbar navbar-expand-lg bg-secondary">
       <div className="container-fluid">
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            {links.map((link) => link)}
+            {links.map((link) => (
+              <li key={link.key} className="nav-item">
+                <NavLink
+                  to={link.to}
+                  className={({ isActive }) =>
+                    isActive ? "nav-link text-light active" : "nav-link text-light"
+                  }
+                  exact
+                >
+                  {link.label}
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
