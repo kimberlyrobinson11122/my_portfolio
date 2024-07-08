6e2218898120eb811b5e66a99af2bf7a55f59490
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-07-08 16:26:22 -0700

    fixed navbar

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Header from '../Header';
 
 export default function Navbar({ links }) {
   return (
-    <nav className="navbar navbar-expand-lg bg-secondary">
+    <nav className="navbar navbar-expand-lg bg-secondary d-flex justify-content-between">
       <div className="container-fluid">
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <Header/>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-end">
             {links.map((link) => (
               <li key={link.key} className="nav-item">
                 <NavLink
