746f23a7e9c7463e9448b725c9106afe9aa1580e
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-07-07 23:21:58 -0700

    added header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from './Nav';
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className="text-left">Kimberly Robinson</h1>
+    </header>
+  );
+};
+
+export default Header;
