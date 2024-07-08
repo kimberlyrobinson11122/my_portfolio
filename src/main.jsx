65ab481a9575fad6eb4c35d242a9283d4e80af84
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-07-07 23:25:42 -0700

    minor updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import ReactDOM from 'react-dom/client';
-// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+// bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-// From utils brings in the pages that router will conditionally use to show the appropriate views
 import App from './App';
 import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/HomePage';
@@ -14,7 +13,8 @@ import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 import ResumePage from './pages/ResumePage';
 
-// Define the accessible routes, and which components respond to which URL
+
+// Create a BrowserRouter and pass the routes array to it
 const router = createBrowserRouter([
   {
     path: '/',
@@ -25,32 +25,27 @@ const router = createBrowserRouter([
         index: true,
         element: <HomePage />,
       },
-
       {
         path: 'projects',
         element: <ProjectPage />,
       },
-
       {
         path: 'about',
         element: <AboutPage />,
       },
-      
       {
         path: 'resume',
         element: <ResumePage />,
       },
-      
       {
         path: 'contact',
         element: <ContactPage />,
       },
-
     ],
   },
 ]);
 
-// Render the RouterProvider component
+// RouterProvider component
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
 );
