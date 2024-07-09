40c4331eb70d0e943dee10899dffc58652e785d6
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-07-08 17:01:09 -0700

    fixed header spacing

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -4,11 +4,11 @@ import Header from '../Header';
 
 export default function Navbar({ links }) {
   return (
-    <nav className="navbar navbar-expand-lg bg-secondary d-flex justify-content-between">
+    <nav className="navbar navbar-expand-lg bg-secondary">
       <div className="container-fluid">
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse d-flex justify-content-between" id="navbarSupportedContent">
           <Header/>
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-end">
+          <ul className="navbar-nav mb-2 mb-lg-0 justify-content-between">
             {links.map((link) => (
               <li key={link.key} className="nav-item">
                 <NavLink
